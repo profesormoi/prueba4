8b6cb2e975f10ca006eb79f03c72c2dc6c18e917
profesormoi <profesormoi@gmail.com>
2020-02-04 19:11:28 +0100

    segunda versión
    nuevas cosas
    
    Signed-off-by: profesormoi <profesormoi@gmail.com>

--- miproyecto/src/miproyecto/MiClase.java
+++ miproyecto/src/miproyecto/MiClase.java
@@ -5,6 +5,8 @@ public class MiClase {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
 	}
 
 }
