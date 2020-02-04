fc1e2fa461191e80eb16449aa37563b9ca692f20
profesormoi <profesormoi@gmail.com>
2020-02-04 19:09:26 +0100

    cambios
    primera versión
    
    
    Signed-off-by: profesormoi <profesormoi@gmail.com>

---
+++ miproyecto/src/miproyecto/MiClase.java
@@ -0,0 +1,10 @@
+package miproyecto;
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("hola");
+	}
+
+}
